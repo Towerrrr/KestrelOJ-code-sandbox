680ed2417bf09f459fd7351f4e4dad532161564d
Towerrrr <1611993104@qq.com>
2025-04-07 18:30:02 +0800

    添加基础鉴权逻辑

--- src/main/java/com/t0r/kestrelojcodesandbox/controller/MainController.java
+++ src/main/java/com/t0r/kestrelojcodesandbox/controller/MainController.java
@@ -9,9 +9,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController("/")
 public class MainController {
+
+    // 定义鉴权请求头和密钥
+    private static final String AUTH_REQUEST_HEADER = "auth";
+
+    // todo 加密
+    // todo API 签名认证的方式
+    private static final String AUTH_REQUEST_SECRET = "secretKey";
 
     @Resource
     private JavaNativeCodeSandbox javaNativeCodeSandbox;
@@ -27,7 +36,15 @@ public class MainController {
      * @return
      */
     @PostMapping("/executeCode")
-    ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest) {
+    ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest, HttpServletRequest request,
+                                    HttpServletResponse response) {
+        // 基本认证
+        String authHeader = request.getHeader(AUTH_REQUEST_HEADER);
+        if (!authHeader.equals(AUTH_REQUEST_SECRET)) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
+            return null;
+        }
+
         if (executeCodeRequest == null) {
             throw  new RuntimeException("请求参数为空");
         }
